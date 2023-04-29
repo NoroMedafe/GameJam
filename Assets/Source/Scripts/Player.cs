db97db5c6fa095bc344c9fda6418b731cf528c19
NoroMedafe <danya1515@inbox.ru>
2023-04-29 17:28:21 +0300

    no message

--- Assets/Source/Scripts/Player.cs
+++ Assets/Source/Scripts/Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 [RequireComponent(typeof (InputSystem))]
 public class Player : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField] private float _speed = 3f;
 
     private InputSystem _input;
@@ -14,23 +13,20 @@ public class Player : MonoBehaviour
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _input = GetComponent<InputSystem>();
-=======
-    [SerializeField] private Rigidbody2D _rigidbody;
-    [SerializeField] private float _speed;
+    }
+
     private InputSystem _inputSystem;
 
     private void Awake()
     {
         _inputSystem = GetComponent<InputSystem>();
->>>>>>> Vitya
+
     }
 
     private void FixedUpdate()
     {
-<<<<<<< HEAD
-        _rigidbody.MovePosition(_rigidbody.position + _input.MoveDirection * _speed * Time.fixedDeltaTime);
-=======
-        _rigidbody.velocity = _inputSystem.MoveDirection * _speed;
->>>>>>> Vitya
+
+        //_rigidbody.MovePosition(_rigidbody.position + _input.MoveDirection * _speed * Time.fixedDeltaTime);
+        //_rigidbody.velocity = _inputSystem.MoveDirection * _speed;
     }
 }
